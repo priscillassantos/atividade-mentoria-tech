1c7a8be92cd8187cd2b4e41f231cf88ae70c7a4e
priscillassantos <maria.priscilla@aluno.edu.pi.gov.br>
2025-08-26 14:12:46 -0300

    Add files via upload
    fah / cel

---
+++ primeiraatividade.java
@@ -0,0 +1,13 @@
+public class primeiraatividade {
+
+public static void main(String[] args) {
+
+
+    double fah = 30;
+    double celsius = (5.0 / 9.0) * (fah - 32);
+    System.out.println("temperatura em celsius: " + celsius);
+
+
+}
+
+}
